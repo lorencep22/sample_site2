64e1c04961e22068bc979b0d2dcbb6317bcb9d78
Lorence Palisan <lorence.palisan@fligno.com>
2025-07-12 13:54:09 +0800

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sample_site2/",
+  base: "/sample_site2",
   build: {
     rollupOptions: {
       output: {
